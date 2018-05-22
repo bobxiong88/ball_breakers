9579c64986feeeded8b493e67b201fe8f1608b7e
glassdome-a <36904083+glassdome-a@users.noreply.github.com>
2018-05-21 19:52:56 -0700

    Create brick.h

---
+++ brick.h
@@ -0,0 +1,19 @@
+#ifndef BRICK_H
+#define BRICK_H
+#include <vector>
+using namespace std;
+
+
+
+struct coords{
+	int x = 0;
+	int y = 0;
+};
+
+struct brick{
+	vector<coord> brickCoords;
+		
+};
+
+
+#endif
